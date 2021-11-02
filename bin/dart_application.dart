3615c642388f865c456aa5c4d7736e2e1aa4440a
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2021-11-02 13:06:46 +0100

    int

--- bin/dart_application.dart
+++ bin/dart_application.dart
@@ -1,5 +1,10 @@
 import 'package:dart_application/dart_application.dart' as dart_application;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_application.calculate()}!');
+  try {
+    print('Hello world: ${dart_application.calculate()}!\n'
+        'Args sum: ${arguments.fold<int>(0, (previousValue, element) => previousValue + int.parse(element))}');
+  } catch (e) {
+    print('Error: At least one argument is not an integer!');
+  }
 }
