e8ea4106a233ab873efe5aefd285236c0f9acdaf
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2021-11-02 16:30:42 +0100

    init

--- test/dart_application_test.dart
+++ test/dart_application_test.dart
@@ -1,8 +1,8 @@
-import 'package:dart_application/dart_application.dart';
+import 'package:dart_application/src/dart_application.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
+  test('The calculate function returns 42', () {
     expect(calculate(), 42);
   });
 }
